610c4ff36d5d869088265965693f23a092dafe64
Alastair Smith <100092508+Alastair-F-Smith@users.noreply.github.com>
2024-12-05 14:23:26 +0000

    Implement separate search column to fetch data from fruit API

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 ingredients_list = st.multiselect('Choose up to 5 ingredients', my_dataframe, max_selections = 5)
 
@@ -29,7 +27,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
